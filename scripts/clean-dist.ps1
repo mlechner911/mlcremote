0ffdbe1c4e2de0ad963d524fce0ab0c576ae8820
Michael Lechner <lechner.altenstadt@web.de>
2026-01-15 13:55:38 +0100

    chore: cleanup obsolete scripts

--- scripts/clean-dist.ps1
+++
@@ -1,4 +0,0 @@
-if (Test-Path "build/dist") {
-    Write-Host "Cleaning build/dist..."
-    Remove-Item -Recurse -Force "build/dist"
-}
--- scripts/make-help.ps1
+++
@@ -1,7 +0,0 @@
-$files = @("Makefile", "Makefile.win", "Makefile.unix")
-Get-Content $files -ErrorAction SilentlyContinue | 
-    Select-String "^## " | 
-    ForEach-Object { 
-        $line = $_.Line -replace "^## ", ""
-        Write-Host $line 
-    }
