ea53780a8afeec2621ba161d5d9df6c38ec23e6e
Michael Lechner <lechner.altenstadt@web.de>
2026-02-06 11:06:42 +0100

    bumoed version, moved Dreadme dist so we dont delete it on task clean

---
+++ backend/internal/handlers/version_const.go
@@ -0,0 +1,7 @@
+package handlers
+
+// BackendVersion is the current version of the backend.
+const BackendVersion = "1.4.8"
+
+// FrontendCompatibleVersion is the semver range of compatible frontend versions.
+const FrontendCompatibleVersion = "^1.1"
